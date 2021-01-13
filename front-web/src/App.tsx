20d2f67cea2a941412320752b954e4f09a9456c8
Thiago Castello <thiago_tyupoid@hotmail.com>
2021-01-13 16:26:53 -0300

    add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,13 @@
-import NavBar from "./NavBar/Index";
 import './App.css';
+import NavBar from "./NavBar/Index";
+import Home from "./Home";
+
  function App() {
 
   return (
     <div>
       <NavBar/>
+      <Home/>
     </div>  
   );
 }
